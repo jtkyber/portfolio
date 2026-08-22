648b7df4bdccc510f17510867c354dce17c46b36
Joey Kyber <43868043+jtkyber@users.noreply.github.com>
2026-08-22 15:43:11 -0400

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import glsl from 'vite-plugin-glsl';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss(), glsl()],
-    base: '/portfolio/',
+    base: '/',
 });
